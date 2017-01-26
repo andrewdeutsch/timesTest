49686c5c59a7e8bc41c6814da2c747f8ba15afcb
andrewdeutsch <andrew.deutsch@gmail.com>
2017-01-26 12:22:03 -0500

    version with images

--- myscript.js
+++ myscript.js
@@ -13,77 +13,50 @@ var NYTD = {
         var storyHolder = document.getElementById("storyBits");
 
         //accessing the first batch of content
-        for(var i=0;i<this.data.page.content[1].collections.length; i++){
+        
+            for(var i=0;i<this.data.page.content[1].collections.length; i++){
 
             var cl1 = this.data.page.content[1].collections[i].assets;
             //var image1 = 'http://www.nytimes.com/'+ cl1[i].images[0].types[2].content;
                 for (var k=0; k<cl1.length; k++){
-                    
+                    var header = document.createElement("div");
+                    var summary = document.createElement("div");
+                    var byline = document.createElement("div");
+                    var imageHolder = document.createElement("div");
+                    var oImg = document.createElement("img");
+                    var aTag = document.createElement("a");
 
+                    header.className = "header";
+                    summary.className ="summary";
+                    byline.className = "byline";
+                    imageHolder.id = "imageHolder";
+                    oImg.className = "image";
+                    aTag.setAttribute("href", cl1[k].url);
 
-                    var header = document.createElement('div');
-                    var summary = document.createElement('div');
-                    var byline = document.createElement('div');
-                    var oImg = document.createElement('img');
-                    var aTag = document.createElement('a');
+                    var divs = [header,  byline, summary];         
 
-                    header.className = 'header';
-                    summary.className ='summary';
-                    byline.className = 'byline';
-                   // oImg.className = 'image';
-                    aTag.setAttribute('href', cl1[k].url);
-
-                    var divs = [header, oImg, byline, summary];
-                    
-                    //console.log();
-                    
-                    // var pathToImg = cl1[k].images[0].types[1].content;
-                    // var image1 = 'http://www.nytimes.com/'+ pathToImg; 
-
-                    if (typeof cl1[k].images[0] == 'undefined'){
+                    if (typeof cl1[k].images[0] !== 'undefined'){
                         //console.log('loadIt');
-                    }else{
                         var pathToImg = cl1[k].images[0].types[1].content;
-                        var image1 = 'http://www.nytimes.com/'+ pathToImg; 
-                        console.log(oImg);
+                        var image1 = "http://www.nytimes.com/"+ pathToImg; 
                         oImg.src = image1;
-                        storyHolder.appendChild(oImg); 
+                        imageHolder.appendChild(oImg);
+                    }else  {
+                        
                     }
                     
-
-                   
-
                     var docFrag = document.createDocumentFragment();
 
-                    //console.log(cl1[k].images[0].types[1].content);
-
-                    
-
-                    
-                    
-
                     if (cl1[k].headline == "Only rank in summary collections" ){
-                        header.innerHTML = "";
-                        byline.innerHTML = "";
-                        summary.innerHTML = "";
+                        header.innerHTML = ""; byline.innerHTML = ""; summary.innerHTML = "";
 
                     }else if (cl1[k].headline == undefined){
-                        header.innerHTML = "";
-                        byline.innerHTML = "";
-                        summary.innerHTML = "";
+                        header.innerHTML = ""; byline.innerHTML = ""; summary.innerHTML = "";
+
                     }
                     else {
                         aTag.innerHTML = cl1[k].headline;
                         header.appendChild(aTag);
-                        // if(oImg.src == undefined){
-
-                        // }else{
-                        //     oImg.src = image1;
-                        // }
-                        
-                        //oImg.appendChild(image1);
-                        //oImg.appendChild(image1);
-                        //oImg.innerHTML = 'http://www.nytimes.com/'+ cl1[k].images[0].types[2].content;
                         byline.innerHTML = cl1[k].byline;
                         summary.innerHTML = cl1[k].summary;
                         for(var l = 0; l < divs.length; l++) {
@@ -92,56 +65,78 @@ var NYTD = {
                         }
                     }
                 }
-      
-       
-    }
+            }
+        
+   
         //accessing the second batch of content
-        for(var j=0;j<this.data.page.content[2].collections.length; j++){
-            var cl2 = this.data.page.content[2].collections[j].assets;
-                for (var m=0; m<cl2.length; m++){
-                    var header = document.createElement('div');
-                    var summary = document.createElement('div');
-                    var aTag = document.createElement('a');
-                    var byline = document.createElement('div');
+        
+                for(var j=0;j<this.data.page.content[2].collections.length; j++){
+                console.log('Oh Hey');
+                var cl2 = this.data.page.content[2].collections[j].assets;
+                    for (var m=0; m<cl2.length; m++){
 
-                    header.className = 'header';
-                    summary.className ='summary';
-                    byline.className = 'byline';
-                    aTag.setAttribute('href', cl2[m].url);
+                        var header = document.createElement("div");
+                        var summary = document.createElement("div");
+                        var byline = document.createElement("div");
+                        var imageHolder = document.createElement("div");
+                        var oImg = document.createElement("img");
+                        var aTag = document.createElement('a');
 
-                    var divs = [header, byline, summary];
+                        header.className = "header";
+                        summary.className ="summary";
+                        byline.className = "byline";
+                        imageHolder.id = "imageHolder";
+                        oImg.className = "image";
+                        aTag.setAttribute("href", cl2[m].url);
 
-                    var docFrag = document.createDocumentFragment();
-                    
-                     if (cl2[m].headline == "Only rank in summary collections" ){ 
-                        header.innerHTML = "";
-                        byline.innerHTML = "";
-                        summary.innerHTML = "";
+                        var divs = [header, byline,  summary];
 
-                    }else if (cl2[m].headline == undefined){
-                        header.innerHTML = "";
-                        byline.innerHTML = "";
-                        summary.innerHTML = "";
-                    }
-                    else if (cl2[m].byline == undefined){
-                        console.log('cl2[m].byline is undefined');
-                        byline.innerHTML = "";
-                    }
-                    else{
-                        aTag.innerHTML = cl2[m].headline;
-                        header.appendChild(aTag);
-                        byline.innerHTML = cl2[m].byline;
-                        summary.innerHTML = cl2[m].summary;
+                        if (typeof cl2[m].images[0] !== 'undefined'){
+                            //console.log('loadIt');
+                            var pathToImg = cl2[m].images[0].types[1].content;
+                            var image1 = 'http://www.nytimes.com/'+ pathToImg; 
+                            oImg.src = image1;
+                            imageHolder.appendChild(oImg);
+                            
+                            imageHolder.setAttribute('style', 'width: 75px');
 
-                        for(var l = 0; l < divs.length; l++) {
-                          docFrag.appendChild(divs[l]);
-                          storyHolder.appendChild(docFrag );
+                        }else  {
+                            
                         }
-  
-                }
 
-        }
+                        var docFrag = document.createDocumentFragment();
+                        
+                         if (cl2[m].headline == "Only rank in summary collections"){ 
+                            header.innerHTML = "";
+                            byline.innerHTML = "";
+                            summary.innerHTML = "";
 
-      }
+                        }else if (cl2[m].headline == undefined){
+                            header.innerHTML = "";
+                            byline.innerHTML = "";
+                            summary.innerHTML = "";
+                        }
+                        else if (cl2[m].byline == undefined){
+                            console.log('cl2[m].byline is undefined');
+                            byline.innerHTML = "";
+                        }
+                        else{
+                            aTag.innerHTML = cl2[m].headline;
+                            header.appendChild(aTag);
+                            byline.innerHTML = cl2[m].byline;
+                            summary.innerHTML = cl2[m].summary;
+
+                            for(var l = 0; l < divs.length; l++) {
+                              docFrag.appendChild(divs[l]);
+                              storyHolder.appendChild(docFrag );
+                            }
+      
+                    }
+
+            }
+
+          }
     }
 }
+    
+    
